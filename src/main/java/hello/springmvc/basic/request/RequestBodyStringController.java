874a797659c1fde2f929c0b007bee6195bd511b7
dbslzld15 <dbslzld15@naver.com>
2021-12-18 16:40:02 +0900

    HTTP 요청 메세지 - 단순 텍스트 추가

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -1,14 +1,19 @@
 package hello.springmvc.basic.request;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
@@ -33,5 +38,34 @@ public class RequestBodyStringController {
         log.info("messageBody={}", messageBody);
 
         response.getWriter().write("ok");
+    }
+
+    @PostMapping("/request-body-string-v3")
+    public void requestBodyStringV3(InputStream inputStream, Writer responseWriter) throws IOException {
+
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+
+        responseWriter.write("ok");
+    }
+
+    @PostMapping("/request-body-string-v3-new")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+
+        String messageBody = httpEntity.getBody();
+
+        log.info("messageBody={}", messageBody);
+
+        return new HttpEntity<>("ok");
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String reuqestBodyStringV4(@RequestBody String messageBody) {
+
+        log.info("messageBody={}", messageBody);
+
+        return "ok";
     }
 }
